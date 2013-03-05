6a4515282f09302bf91bd0d89890692d23f233cc
Michael Kitson <michaelskitson@gmail.com>
2013-03-05 08:12:35 -0500

    Initial code commit

---
+++ songza.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Watches a folder (like a browser cache) for mp4 files
+# When one is found, it renames it and places it into a music library at
+# artist/album/title.m4a
+#
+# Requires avconv, iconv, and inotifywait to be installed and $HOME to be set
+
+CACHE_FOLDER="$HOME/.cache/chromium/Default/Cache"
+SONG_STORE="$HOME/Music/songza"
+inotifywait -m -e close_write --format '%f' $CACHE_FOLDER | while read line
+do
+    original="$CACHE_FOLDER/$line"
+    if [ -n `file $original | \grep --count 'MPEG v4'` ]; then
+        metadata=$(avconv -i $original 2>&1)
+        title=$(echo "$metadata" | \grep title | cut -d':' -f 2 | sed 's/^ //' | iconv -f WINDOWS-1252)
+        artist=$(echo "$metadata" | \grep artist | cut -d':' -f 2 | sed 's/^ //' | iconv -f WINDOWS-1252)
+        album=$(echo "$metadata" | \grep album | cut -d':' -f 2 | sed 's/^ //' | iconv -f WINDOWS-1252)
+        if [[ -n $title && -n $artist && -n $album ]]; then
+            folder="$SONG_STORE/$artist/$album"
+            newFile="$folder/$title.m4a"
+            if [ ! -f "$newFile" ]; then
+                mkdir -p "$folder"
+                cp "$original" "$newFile"
+            fi
+        fi
+    fi
+done
